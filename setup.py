b306461c8cd36e651adf5ca1bffc9485111c0aca
JR-1991 <range.jan@web.de>
2022-07-10 17:30:15 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="sdRDM",
-    version="0.0.1",
+    version="0.0.2",
     author="Range, Jan",
     author_email="jan.range@simtech.uni-stuttgart.de",
     license="MIT License",
