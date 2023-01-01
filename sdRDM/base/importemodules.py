d8f5dd24c7689b1de8e636a556ad0dae39dd0074
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-01-01 14:22:48 +0100

    Added __repr__ method

--- sdRDM/base/importemodules.py
+++ sdRDM/base/importemodules.py
@@ -1,10 +1,18 @@
+import textwrap
+
 from typing import Dict, Optional
 
 
 class ImportedModules:
     """Empty class used to store all sub classes"""
 
-    def __init__(self, classes, enums=None, links: Optional[Dict] = None):
+    def __init__(
+        self,
+        classes,
+        enums: Optional["ImportedModules"] = None,
+        links: Optional[Dict] = None,
+        include_links: bool = True,
+    ):
         for name, node in classes.items():
             if hasattr(node, "cls"):
                 # Add all classes
@@ -17,11 +25,12 @@ class ImportedModules:
                 setattr(self, name, node)
 
         if enums:
-            self.enums = self.__class__(classes=enums)
+            self.enums = self.__class__(classes=enums, include_links=False)
 
         # Process links
-        self.links = links
-        self._distribute_links()
+        if include_links:
+            self.links = links
+            self._distribute_links()
 
     def _distribute_links(self):
         """Adds the given links as instance methods"""
@@ -33,3 +42,38 @@ class ImportedModules:
             obj = getattr(self, link["__model__"])
             converter = lambda self: self.convert_to(template=link)[0]
             setattr(obj, f"to_{name.replace(' ', '_')}", converter)
+
+    def __repr__(self) -> str:
+
+        GROUPS = ["Objects", "enums", "links"]
+        MAXINDENT = len("Objects ")
+        out_string = []
+
+        for group in GROUPS:
+            prefix = (
+                f"\033[96m{group.capitalize()}\033[0m{' ' * (MAXINDENT - len(group))}"
+            )
+            wrapper = textwrap.TextWrapper(
+                initial_indent=prefix, width=100, subsequent_indent="        "
+            )
+
+            if group not in self.__dict__:
+                out_string += [
+                    wrapper.fill(
+                        ", ".join(
+                            [name for name in self.__dict__ if name not in GROUPS]
+                        )
+                    )
+                ]
+            elif group == "enums":
+                out_string += [
+                    wrapper.fill(
+                        ", ".join([name for name in getattr(self, group).__dict__])
+                    )
+                ]
+            elif group == "links":
+                out_string += [
+                    wrapper.fill(", ".join([name for name in getattr(self, group)]))
+                ]
+
+        return "\n".join(out_string)
