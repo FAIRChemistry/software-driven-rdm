c61e32edcdb637f68b69033311142f1a3b89c115
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-01-20 01:29:27 +0100

    Fixed entries dont get a parent

--- sdRDM/base/listplus.py
+++ sdRDM/base/listplus.py
@@ -15,6 +15,9 @@ class ListPlus(List[Any]):
     def __init__(self, *args, **kwargs):
         super(ListPlus, self).__init__()
 
+        self.__parent__ = None
+        self.__attribute__ = None
+
         for arg in args:
             if "generator object" in repr(arg):
                 for element in list(arg):
@@ -24,11 +27,30 @@ class ListPlus(List[Any]):
 
     def append(self, *args):
         for arg in args:
-            if hasattr(arg, "__fields__"):
+            if hasattr(arg, "__fields__") and self.is_part_of_model():
                 arg.__parent__ = self.__parent__
                 arg.check_references(self.__attribute__, arg)
 
             super().append(arg)
+
+    def is_part_of_model(self) -> bool:
+        """Checks whether this list is already integrated"""
+        return self.__parent__ is not None and self.__attribute__ is not None
+
+    def __setattr__(self, name: str, value: Any) -> None:
+
+        if name == "__parent__" and value is not None:
+            self.set_parent_for_object_entries(value)
+
+        return super().__setattr__(name, value)
+
+    def set_parent_for_object_entries(self, parent):
+        """Adds parent relation so object entries once it has been set"""
+        for entry in self:
+            if not hasattr(entry, "__fields__"):
+                continue
+
+            entry.__parent__ = parent
 
     def get(self, query: str, attr: str = "id"):
         """Given an a query, returns all objects that match
