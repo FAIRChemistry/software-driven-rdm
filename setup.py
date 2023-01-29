36bcb8aa2636ebedc74220e40e3e551fa86eec21
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-01-29 16:39:57 +0100

    Added dependency

--- setup.py
+++ setup.py
@@ -32,7 +32,8 @@ setup(
         "termcolor",
         "redbaron==0.9.2",
         "dotted-dict==1.1.3",
-        "markdown-it-py==2.1.0"
+        "markdown-it-py==2.1.0",
+        "autoflake==2.0.0",
     ],
     extras_require={"test": ["pytest"], "dataverse": ["easyDataverse"]},
 )
