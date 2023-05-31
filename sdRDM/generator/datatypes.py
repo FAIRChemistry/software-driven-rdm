aa7a061d8e7186b82b942b3c0e1e7d767f6281a5
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-05-31 15:48:44 +0200

    Fixed type imports

--- sdRDM/generator/datatypes.py
+++ sdRDM/generator/datatypes.py
@@ -20,17 +20,15 @@ class DataTypes(Enum):
     anyhttpurl = ("AnyHttpUrl", ["from pydantic import AnyHttpUrl"])
     AnyUrl = ("AnyUrl", ["from pydantic import AnyUrl"])
     URL = ("AnyUrl", ["from pydantic import AnyUrl"])
-    posfloat = ("PositiveFloat", ["from pydantic.types import PositiveFloat"])
-    PositiveFloat = ("PositiveFloat", ["from pydantic.types import PositiveFloat"])
-    positivefloat = ("PositiveFloat", ["from pydantic.types import PositiveFloat"])
+    posfloat = ("PositiveFloat", ["from pydantic import PositiveFloat"])
+    PositiveFloat = ("PositiveFloat", ["from pydantic import PositiveFloat"])
+    positivefloat = ("PositiveFloat", ["from pydantic import PositiveFloat"])
     date = ("date", ["from datetime import date"])
     datetime = ("datetime", ["from datetime import datetime"])
     bool = ("bool", None)
     boolean = ("bool", None)
     Decimal = ("Decimal", ["from pydantic.types import Decimal"])
     decimal = ("Decimal", ["from pydantic.types import Decimal"])
-    Enum = ("Enum", ["from pydantic.types import Enum"])
-    enum = ("Enum", ["from pydantic.types import Enum"])
     Path = ("Path", ["from pydantic.types import Path"])
     path = ("Path", ["from pydantic.types import Path"])
     Any = ("Any", ["from typing import Any"])
@@ -39,104 +37,82 @@ class DataTypes(Enum):
     callable = ("Callable", ["from pydantic.types import Callable"])
     FrozenSet = ("FrozenSet", ["from pydantic.types import FrozenSet"])
     frozenset = ("FrozenSet", ["from pydantic.types import FrozenSet"])
-    Optional = ("Optional", ["from pydantic.types import Optional"])
-    optional = ("Optional", ["from pydantic.types import Optional"])
     Pattern = ("Pattern", ["from pydantic.types import Pattern"])
     pattern = ("Pattern", ["from pydantic.types import Pattern"])
     UUID = ("UUID", ["from pydantic.types import UUID"])
     uuid = ("UUID", ["from pydantic.types import UUID"])
-    NoneStr = ("NoneStr", ["from pydantic.types import NoneStr"])
-    nonestr = ("NoneStr", ["from pydantic.types import NoneStr"])
-    NoneBytes = ("NoneBytes", ["from pydantic.types import NoneBytes"])
-    nonebytes = ("NoneBytes", ["from pydantic.types import NoneBytes"])
-    StrBytes = ("StrBytes", ["from pydantic.types import StrBytes"])
-    strbytes = ("StrBytes", ["from pydantic.types import StrBytes"])
-    NoneStrBytes = ("NoneStrBytes", ["from pydantic.types import NoneStrBytes"])
-    nonestrbytes = ("NoneStrBytes", ["from pydantic.types import NoneStrBytes"])
-    OptionalInt = ("OptionalInt", ["from pydantic.types import OptionalInt"])
-    optionalint = ("OptionalInt", ["from pydantic.types import OptionalInt"])
-    OptionalIntFloat = (
-        "OptionalIntFloat",
-        ["from pydantic.types import OptionalIntFloat"],
-    )
-    optionalintfloat = (
-        "OptionalIntFloat",
-        ["from pydantic.types import OptionalIntFloat"],
-    )
-    OptionalIntFloatDecimal = (
-        "OptionalIntFloatDecimal",
-        ["from pydantic.types import OptionalIntFloatDecimal"],
-    )
-    optionalintfloatdecimal = (
-        "OptionalIntFloatDecimal",
-        [
-            "from pydantic.types import OptionalIntFloatDecimal",
-        ],
-    )
+    NoneStr = ("NoneStr", ["from pydantic import NoneStr"])
+    nonestr = ("NoneStr", ["from pydantic import NoneStr"])
+    NoneBytes = ("NoneBytes", ["from pydantic import NoneBytes"])
+    nonebytes = ("NoneBytes", ["from pydantic import NoneBytes"])
+    StrBytes = ("StrBytes", ["from pydantic import StrBytes"])
+    strbytes = ("StrBytes", ["from pydantic import StrBytes"])
+    NoneStrBytes = ("NoneStrBytes", ["from pydantic import NoneStrBytes"])
+    nonestrbytes = ("NoneStrBytes", ["from pydantic import NoneStrBytes"])
     StrIntFloat = ("StrIntFloat", ["from pydantic.types import StrIntFloat"])
     strintfloat = ("StrIntFloat", ["from pydantic.types import StrIntFloat"])
-    StrictBool = ("StrictBool", ["from pydantic.types import StrictBool"])
-    strictbool = ("StrictBool", ["from pydantic.types import StrictBool"])
-    PositiveInt = ("PositiveInt", ["from pydantic.types import PositiveInt"])
-    positiveint = ("PositiveInt", ["from pydantic.types import PositiveInt"])
-    NegativeInt = ("NegativeInt", ["from pydantic.types import NegativeInt"])
-    negativeint = ("NegativeInt", ["from pydantic.types import NegativeInt"])
-    NonPositiveInt = ("NonPositiveInt", ["from pydantic.types import NonPositiveInt"])
-    nonpositiveint = ("NonPositiveInt", ["from pydantic.types import NonPositiveInt"])
-    NonNegativeInt = ("NonNegativeInt", ["from pydantic.types import NonNegativeInt"])
-    nonnegativeint = ("NonNegativeInt", ["from pydantic.types import NonNegativeInt"])
-    StrictInt = ("StrictInt", ["from pydantic.types import StrictInt"])
-    strictint = ("StrictInt", ["from pydantic.types import StrictInt"])
+    StrictBool = ("StrictBool", ["from pydantic import StrictBool"])
+    strictbool = ("StrictBool", ["from pydantic import StrictBool"])
+    PositiveInt = ("PositiveInt", ["from pydantic import PositiveInt"])
+    positiveint = ("PositiveInt", ["from pydantic import PositiveInt"])
+    NegativeInt = ("NegativeInt", ["from pydantic import NegativeInt"])
+    negativeint = ("NegativeInt", ["from pydantic import NegativeInt"])
+    NonPositiveInt = ("NonPositiveInt", ["from pydantic import NonPositiveInt"])
+    nonpositiveint = ("NonPositiveInt", ["from pydantic import NonPositiveInt"])
+    NonNegativeInt = ("NonNegativeInt", ["from pydantic import NonNegativeInt"])
+    nonnegativeint = ("NonNegativeInt", ["from pydantic import NonNegativeInt"])
+    StrictInt = ("StrictInt", ["from pydantic import StrictInt"])
+    strictint = ("StrictInt", ["from pydantic import StrictInt"])
     ConstrainedFloat = (
         "ConstrainedFloat",
-        ["from pydantic.types import ConstrainedFloat"],
+        ["from pydantic import ConstrainedFloat"],
     )
     constrainedfloat = (
         "ConstrainedFloat",
-        ["from pydantic.types import ConstrainedFloat"],
+        ["from pydantic import ConstrainedFloat"],
     )
-    NegativeFloat = ("NegativeFloat", ["from pydantic.types import NegativeFloat"])
-    negativefloat = ("NegativeFloat", ["from pydantic.types import NegativeFloat"])
+    NegativeFloat = ("NegativeFloat", ["from pydantic import NegativeFloat"])
+    negativefloat = ("NegativeFloat", ["from pydantic import NegativeFloat"])
     NonPositiveFloat = (
         "NonPositiveFloat",
-        ["from pydantic.types import NonPositiveFloat"],
+        ["from pydantic import NonPositiveFloat"],
     )
     nonpositivefloat = (
         "NonPositiveFloat",
-        ["from pydantic.types import NonPositiveFloat"],
+        ["from pydantic import NonPositiveFloat"],
     )
     NonNegativeFloat = (
         "NonNegativeFloat",
-        ["from pydantic.types import NonNegativeFloat"],
+        ["from pydantic import NonNegativeFloat"],
     )
     nonnegativefloat = (
         "NonNegativeFloat",
-        ["from pydantic.types import NonNegativeFloat"],
+        ["from pydantic import NonNegativeFloat"],
     )
-    StrictFloat = ("StrictFloat", ["from pydantic.types import StrictFloat"])
-    strictfloat = ("StrictFloat", ["from pydantic.types import StrictFloat"])
-    StrictBytes = ("StrictBytes", ["from pydantic.types import StrictBytes"])
-    strictbytes = ("StrictBytes", ["from pydantic.types import StrictBytes"])
-    StrictStr = ("StrictStr", ["from pydantic.types import StrictStr"])
-    strictstr = ("StrictStr", ["from pydantic.types import StrictStr"])
-    UUID1 = ("UUID1", ["from pydantic.types import UUID1"])
-    uuid1 = ("UUID1", ["from pydantic.types import UUID1"])
-    UUID3 = ("UUID3", ["from pydantic.types import UUID3"])
-    uuid3 = ("UUID3", ["from pydantic.types import UUID3"])
-    UUID4 = ("UUID4", ["from pydantic.types import UUID4"])
-    uuid4 = ("UUID4", ["from pydantic.types import UUID4"])
-    UUID5 = ("UUID5", ["from pydantic.types import UUID5"])
-    uuid5 = ("UUID5", ["from pydantic.types import UUID5"])
-    FilePath = ("FilePath", ["from pydantic.types import FilePath"])
-    filepath = ("FilePath", ["from pydantic.types import FilePath"])
-    DirectoryPath = ("DirectoryPath", ["from pydantic.types import DirectoryPath"])
-    directorypath = ("DirectoryPath", ["from pydantic.types import DirectoryPath"])
-    Json = ("Json", ["from pydantic.types import Json"])
-    json = ("Json", ["from pydantic.types import Json"])
-    PastDate = ("PastDate", ["from pydantic.types import PastDate"])
-    pastdate = ("PastDate", ["from pydantic.types import PastDate"])
-    FutureDate = ("FutureDate", ["from pydantic.types import FutureDate"])
-    futuredate = ("FutureDate", ["from pydantic.types import FutureDate"])
+    StrictFloat = ("StrictFloat", ["from pydantic import StrictFloat"])
+    strictfloat = ("StrictFloat", ["from pydantic import StrictFloat"])
+    StrictBytes = ("StrictBytes", ["from pydantic import StrictBytes"])
+    strictbytes = ("StrictBytes", ["from pydantic import StrictBytes"])
+    StrictStr = ("StrictStr", ["from pydantic import StrictStr"])
+    strictstr = ("StrictStr", ["from pydantic import StrictStr"])
+    UUID1 = ("UUID1", ["from pydantic import UUID1"])
+    uuid1 = ("UUID1", ["from pydantic import UUID1"])
+    UUID3 = ("UUID3", ["from pydantic import UUID3"])
+    uuid3 = ("UUID3", ["from pydantic import UUID3"])
+    UUID4 = ("UUID4", ["from pydantic import UUID4"])
+    uuid4 = ("UUID4", ["from pydantic import UUID4"])
+    UUID5 = ("UUID5", ["from pydantic import UUID5"])
+    uuid5 = ("UUID5", ["from pydantic import UUID5"])
+    FilePath = ("FilePath", ["from pydantic import FilePath"])
+    filepath = ("FilePath", ["from pydantic import FilePath"])
+    DirectoryPath = ("DirectoryPath", ["from pydantic import DirectoryPath"])
+    directorypath = ("DirectoryPath", ["from pydantic import DirectoryPath"])
+    Json = ("Json", ["from pydantic import Json"])
+    json = ("Json", ["from pydantic import Json"])
+    PastDate = ("PastDate", ["from pydantic import PastDate"])
+    pastdate = ("PastDate", ["from pydantic import PastDate"])
+    FutureDate = ("FutureDate", ["from pydantic import FutureDate"])
+    futuredate = ("FutureDate", ["from pydantic import FutureDate"])
     NDArray = (
         "Union[NDArray, H5Dataset]",
         [
