347fb85d26ce78bc5f34ab4e9218a73f56923f14
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-10-20 19:38:51 +0200

    Bumped version patch

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="sdRDM",
-    version="0.0.4",
+    version="0.0.5",
     author="Range, Jan",
     author_email="jan.range@simtech.uni-stuttgart.de",
     license="MIT License",
