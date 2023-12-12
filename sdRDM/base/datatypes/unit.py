0ccc179f9475cc041fa704d259bfa9e42bc60735
Jan Range <range.jan@web.de>
2023-12-12 01:48:33 +0100

    build composite unit and add hash

--- sdRDM/base/datatypes/unit.py
+++ sdRDM/base/datatypes/unit.py
@@ -1,8 +1,8 @@
 import sdRDM
 
 from typing import Union
-from astropy.units import UnitBase, Unit
-from pydantic import field_serializer, PrivateAttr
+from astropy.units import UnitBase, Unit, CompositeUnit
+from pydantic import field_serializer, field_validator, PrivateAttr
 
 
 class UnitType(sdRDM.DataModel):
@@ -26,6 +26,31 @@ class UnitType(sdRDM.DataModel):
     bases: list[Union[str, UnitBase]]
     powers: list[float]
     _unit: Union[UnitBase, UnitBase] = PrivateAttr()
+    _hash: int = PrivateAttr()
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self._unit = CompositeUnit(
+            scale=self.scale,
+            bases=self.bases,
+            powers=self.powers,
+        )
+
+        self._hash = hash(self._unit)
+
+    @field_validator("bases", mode="before")
+    @classmethod
+    def _bases_to_astropy(cls, v):
+        """
+        Convert a list of bases to Astropy units.
+
+        Args:
+            v (list): List of bases.
+
+        Returns:
+            list: List of Astropy units.
+        """
+        return [Unit(base) for base in v]
 
     @field_serializer("bases")
     def _serialize_bases(self, bases: list[Union[str, UnitBase]]):
