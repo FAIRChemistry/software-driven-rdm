1813e8d0574db0f5df2c480d1d1d117ed1ac4ebb
Jan Range <range.jan@web.de>
2024-02-15 13:49:50 +0100

    adjust to `Unit` system and add model validator

--- sdRDM/base/datatypes/quantity.py
+++ sdRDM/base/datatypes/quantity.py
@@ -1,8 +1,8 @@
 from typing import List, Union
 from uuid import uuid4
 
-from pydantic import PrivateAttr, computed_field
-from pydantic_xml import attr, element, wrapped
+from pydantic import PrivateAttr, computed_field, model_validator
+from pydantic_xml import attr, element
 import sdRDM
 from sdRDM.base.datatypes.unit import Unit
 
@@ -26,10 +26,6 @@ class Quantity(
         id (str): The unique identifier of the quantity.
         _quantity (AstroQuantity): The underlying astropy quantity object.
 
-    Properties:
-        value (Union[float, int]): The numerical value of the quantity.
-        unit (astropy.units.UnitBase): The unit of measurement for the quantity.
-
     Methods:
         __mul__(self, other): Multiply the quantity by another quantity or a scalar.
         __truediv__(self, other): Divide the quantity by another quantity or a scalar.
@@ -38,35 +34,21 @@ class Quantity(
     """
 
     id: str = attr(name="id", default_factory=lambda: str(uuid4()))
-    _quantity: AstroQuantity = PrivateAttr()
+    value: Union[float, int] = attr(
+        title="Value",
+        description="The numerical value of the quantity.",
+    )
 
-    def __init__(
-        self,
-        value: Union[float, int],
-        unit: Union[str, astropy.units.UnitBase],
-    ):
-        super().__init__()
-        if isinstance(unit, str):
-            unit = astropy.units.Unit(unit)
-        self._quantity = value * unit
+    unit: Unit = element(
+        title="Unit",
+        description="The unit of measurement for the quantity.",
+    )
 
-    @computed_field
-    def value(self) -> Union[float, int]:
-        return self._quantity.value
+    _quantity: AstroQuantity = PrivateAttr(default=None)
 
-    @value.setter
-    def value(self, value):
-        self._quantity = value * self._quantity.unit
-
-    @computed_field
-    def unit(self) -> astropy.units.UnitBase:
-        return Unit.from_string(str(self._quantity.unit))
-
-    @unit.setter
-    def unit(self, unit):
-        if isinstance(unit, str):
-            unit = astropy.units.Unit(unit)
-        self._quantity = self._quantity.to(unit)
+    @model_validator(mode="after")
+    def _set_quantity(self):
+        self._quantity = AstroQuantity(self.value, self.unit._unit)
 
     # dunders
     def __mul__(self, other):
@@ -111,3 +93,19 @@ class Quantity(
         if isinstance(other, (int, float)):
             new_quantity = self._quantity - other
             return Quantity(value=new_quantity.value, unit=new_quantity.unit)
+
+
+if __name__ == "__main__":
+    quantity = Quantity(value=1, unit="m")
+
+    # JSON
+    print(quantity.json(indent=2), end="\n\n")
+
+    # XML
+    print(quantity.xml(), end="\n\n")
+
+    # YAML
+    print(quantity.yaml(), end="\n\n")
+
+    # Astropy Quantity object
+    print("Quantity object: ", quantity._quantity, end="\n\n")
